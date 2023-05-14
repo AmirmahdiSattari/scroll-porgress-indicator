129f7c58f412f43262f7077e58f052e2612da3aa
Amirmahdi Sattari <amirmahdisattariams@gmail.com>
2023-05-14 23:32:16 +0330

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const progress =  document.querySelector(".progress-bar");
+
+
+window.addEventListener('scroll',()=>{
+    const winScroll = window.pageYOffset;
+    const height = document.documentElement.scrollHeight-window.innerHeight
+    const scrolled =(winScroll/height) * 100;
+    progress.style.width =`${scrolled}%`;
+
+});
